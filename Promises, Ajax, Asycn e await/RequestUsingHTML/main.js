b74c6be421e78ce225c4bf17a3f3462074ced68c
Gil Andrey <gil_andrey@hotmail.com>
2025-04-13 10:57:12 -0300

    Fetch API (GET)

--- Promises, Ajax, Asycn e await/RequestUsingHTML/main.js
+++ Promises, Ajax, Asycn e await/RequestUsingHTML/main.js
@@ -1,21 +1,21 @@
-const request = (obj) => {
-  return new Promise((resolve, reject) => {
-    // xhr = XML HTTP REQUEST -> Esse tipo de nome é padrão
-    const xhr = new XMLHttpRequest();
-    // xhr.open = GET
-    xhr.open(obj.method, obj.url, true);
-    // xhr.send = POST
-    xhr.send();
+// const request = (obj) => {
+//   return new Promise((resolve, reject) => {
+//     // xhr = XML HTTP REQUEST -> Esse tipo de nome é padrão
+//     const xhr = new XMLHttpRequest();
+//     // xhr.open = GET
+//     xhr.open(obj.method, obj.url, true);
+//     // xhr.send = POST
+//     xhr.send();
 
-    xhr.addEventListener("load", () => {
-      if (xhr.status >= 200 && xhr.status < 300) {
-        resolve(xhr.responseText);
-      } else {
-        reject(xhr.statusText);
-      }
-    });
-  });
-};
+//     xhr.addEventListener("load", () => {
+//       if (xhr.status >= 200 && xhr.status < 300) {
+//         resolve(xhr.responseText);
+//       } else {
+//         reject(xhr.statusText);
+//       }
+//     });
+//   });
+// };
 
 document.addEventListener("click", (e) => {
   const el = e.target;
@@ -28,18 +28,16 @@ document.addEventListener("click", (e) => {
 });
 
 async function carregaPagina(el) {
-  const href = el.getAttribute("href");
-
-  const objConfig = {
-    method: "GET",
-    url: href,
-  };
-
   try {
-    const response = await request(objConfig);
-    carregaResultado(response);
-  } catch (error) {
-    console.log(error);
+    const href = el.getAttribute("href");
+    const response = await fetch(href);
+
+    if (response.status !== 200) throw new Error("ERROR 404");
+
+    const html = await response.text();
+    carregaResultado(html);
+  } catch (e) {
+    console.warn(e);
   }
 }
 
@@ -47,3 +45,11 @@ function carregaResultado(response) {
   const resultado = document.querySelector(".resultado");
   resultado.innerHTML = response;
 }
+
+fetch("pagina2.html")
+  .then((resposta) => {
+    if (resposta.status !== 200) throw new Error("ERRO 404 NOSSO");
+    return resposta.text();
+  })
+  .then((html) => console.log(html))
+  .catch((e) => console.error(e));
