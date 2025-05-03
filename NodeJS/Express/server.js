7856cf3b22608b9930144b65817f0e4fff79a1ac
Gil Andrey <gil_andrey@hotmail.com>
2025-05-03 15:16:35 -0300

    MongoDB - Conexão e primeiro Model (commit exemplo)

--- NodeJS/Express/server.js
+++ NodeJS/Express/server.js
@@ -1,10 +1,23 @@
 // Para aplicação do Nodemon, é necessario o comando "npm install nodemon --save-dev", para rodar = npx nodemon "arquivo.js"
+require("dotenv").config();
 
 const express = require("express");
 const app = express();
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.CONNECTIONSTRING)
+  .then(() => {
+    console.log("Conectei a base de dados!!");
+    app.emit("pronto");
+  })
+  .catch((e) => console.log(e));
+
 const routes = require("./routes");
 const path = require("path");
 const { middlewareGlobal } = require("./src/middlewares/middleware");
+const { connect } = require("http2");
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -18,7 +31,9 @@ app.set("view engine", "ejs");
 app.use(middlewareGlobal);
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log("Acessar http://localhost:3000");
-  console.log("Servidor executando na porta 3000");
+app.on("pronto", () => {
+  app.listen(3000, () => {
+    console.log("Acessar http://localhost:3000");
+    console.log("Servidor executando na porta 3000");
+  });
 });
